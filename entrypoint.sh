b52f3c959a8373bdef4a1cab04b37edc564c1056
fatitlem <fatimazahrae.rhmaritlemcani@usmba.ac.ma>
2024-08-29 15:36:11 +0100

    update0821ksjdhk

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,7 @@
 # create a db and a table:
 echo "Initializing database..."
 mysql -u root -pmysql <<-EOSQL
+CREATE DATABASE IF NOT EXISTS my_database;
+USE my_database;
 EOSQL
 echo "Database initialized."
